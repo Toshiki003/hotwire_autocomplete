5047006bb602330a8355a9c872d252241b1d7604
関目智史 <zou3road@gmail.com>
2023-03-24 22:47:50 +0900

    seed_create

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 150.times do
-  Faker::Movie.unique.title
+  Movie.create(title: Faker::Movie.unique.title)
 end
 puts "150 movies created"
