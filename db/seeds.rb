47eb03395c28106694c90ef9a9351ef825fbd2b7
関目智史 <zou3road@gmail.com>
2023-03-24 22:42:19 +0900

    scaffold_movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+150.times do
+  Faker::Movie.unique.title
+end
+puts "150 movies created"
